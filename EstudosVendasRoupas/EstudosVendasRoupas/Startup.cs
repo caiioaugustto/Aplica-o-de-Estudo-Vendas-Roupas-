5804f166d780d29258a40115ff15081618504097
Caio Augusto <caiio_augustto@yahoo.com.br>
2016-07-18 19:22:07 -0300

    Criação do Projeto MVC.

---
+++ EstudosVendasRoupas/EstudosVendasRoupas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstudosVendasRoupas.Startup))]
+namespace EstudosVendasRoupas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
