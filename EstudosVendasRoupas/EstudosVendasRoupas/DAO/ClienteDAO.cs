8c48bb0bacb3467a2b182d8fda4e605c1152a366
Caio Augusto <caiio_augustto@yahoo.com.br>
2016-07-18 21:19:31 -0300

    Criação de pasta DAO - Acesso ao banco de dados

---
+++ EstudosVendasRoupas/EstudosVendasRoupas/DAO/ClienteDAO.cs
@@ -0,0 +1,47 @@
+﻿using EstudosVendasRoupas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EstudosVendasRoupas.DAO
+{
+    public class ClienteDAO
+    {
+        private Context context;
+
+        public ClienteDAO(Context context)
+        {
+            this.context = context;
+        }
+
+        public void AdicionarCliente(Cliente cliente)
+        {
+            context.Cliente.Add(cliente);
+            context.SaveChanges();
+        }
+
+        public bool VerificarCPFExistente(Cliente cliente)
+        {
+            Cliente clientes = context.Cliente
+                .Where(a => a.CPF == cliente.CPF)
+                .FirstOrDefault();
+
+            if (clientes == null)
+                return false;
+            else
+                return true;
+        }
+        public bool VerificarEmailExistente(Cliente cliente)
+        {
+            Cliente clientes = context.Cliente
+                .Where(a => a.Email == cliente.Email)
+                .FirstOrDefault();
+
+            if (clientes == null)
+                return false;
+            else
+                return true;
+        }
+    }
+}
